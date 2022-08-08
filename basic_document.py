4889cd0385084b936202e0b9021b20c5799a339a
cactar54 <cactar54@aol.com>
2022-08-08 19:10:20 -0400

    Defined docUuid string to allow for storage of UUID4 Defined createDate string to allow for storage of Unix Time

--- basic_document.py
+++ basic_document.py
@@ -3,11 +3,25 @@ import uuid
 from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(frozen=True)
 class BasicDocument:
+    docUuid: str = field(init=False, default_factory=str)
+    createDate: str = field(init=False, default_factory=str)
+
     def __init__(self):
-        self.__id
+        self.__docUuid
         self.__docName
         self.__authorName
         self.__createDate
         self.__company
+
+   # def document_verification(self):
+
+   #     docUuid = uuid.uuid4()
+   #     createDate = time.time()
+
+   #     print(docUuid)
+   #     print(createDate)
+
+
+myDoc = BasicDocument
