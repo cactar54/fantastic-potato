aac88358c83a36ecd50169ecf8503134e5b2ed49
cactar54 <cactar54@aol.com>
2022-08-09 19:57:42 -0400

    Imported json and pathLib Defined load_config private variable Added defining of my_file variable to load_config to check if there is a config file, then load it if we do or return an empty list if there is not added defining of json_dump variable to save_config to save a config json file

--- dict_manager.py
+++ dict_manager.py
@@ -1,12 +1,21 @@
+import json
+import pathlib
 
 class DictManager:
     def __init__(self):
         self.__path
         self.__config
+        self.__load_config()
 
     def __load_config(self):
+        my_file = pathlib.Path("/Documents")
+        if my_file.is_file():
+            my_file = json.load()
+            return my_file
 
+        return []
 
     def __save_config(self):
-
-        
+        with open("config.json", "w") as outfile:
+            json_dump = json.dumps(self.config)
+            outfile.write(json_dump)
